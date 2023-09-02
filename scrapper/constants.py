787fc00e61c897d1b867fe60d5fe5bc5b2160f64
Vincent Adiasor <Dvadiasor@gmail.com>
2023-09-02 14:42:28 +0100

    .

--- scrapper/constants.py
+++ scrapper/constants.py
@@ -4,5 +4,5 @@ BASE_URL = "https://www.tandfonline.com/loi/nvpp20"
 OUT_PATH = os.path.abspath("data/output.xlsx")
 IMAGE_PATH = os.path.abspath("data/Photos")
 LOGIN_PATH = "https://www.tandfonline.com/action/showLogin?uri=%2Floi%2Fnvpp20"
-EMAIL = "ido.koubi@live.biu.ac.il"
-PASSWORD = "bri92#Ga"
+EMAIL = ""
+PASSWORD = ""
